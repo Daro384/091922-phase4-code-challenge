cce5e412aec5cb80ce70e4d0df3fe9ab389666b9
Joseph <joffan98@gmail.com>
2022-12-01 12:27:25 -0600

    submission for deadline

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :powers, only: [:index, :show, :update]
+  resources :hero_powers, only: [:create]
+  resources :heroes, only: [:index, :show, ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
